ab84620f9fbcd0a18bc5bbcc156d7d4f81ade99a
unknown <edesp.lopez17@gmail.com>
2024-07-07 23:29:02 -0700

    file structure is messed up fixing later

---
+++ client/webpack.config.js
@@ -0,0 +1,66 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const WebpackPwaManifest = require('webpack-pwa-manifest');
+const path = require('path');
+const { InjectManifest } = require('workbox-webpack-plugin');
+
+module.exports = () => {
+  return {
+    mode: 'development',
+    entry: {
+      main: './src/js/index.js',
+      install: './src/js/install.js',
+      database: './src/js/database.js',
+      editor: './src/js/editor.js',
+      header: './src/js/header.js',
+    },
+    output: {
+      filename: '[name].bundle.js',
+      path: path.resolve(__dirname, 'dist'),
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: './src/index.html',
+        filename: 'index.html',
+        chunks: ['main'],
+      }),
+      new WebpackPwaManifest({
+        filename: 'manifest.json',
+        name: 'Your future Text Editor',
+        short_name: 'JATE',
+        description: 'Just a Text Editor',
+        background_color: '#ffffff',
+        theme_color: '#2196f3',
+        start_url: '/',
+        icons: [
+          {
+            src: path.resolve(__dirname, 'src/images/logo.png'),
+            sizes: [96, 128, 192, 256, 384, 512],
+            destination: path.join('images', 'logos'),
+          },
+        ],
+      }),
+      new InjectManifest({
+        swSrc: "./src/src-sw.js",
+        swDest: "sw.js",
+      }),
+    ],
+    module: {
+      rules: [
+        {
+          test: /\.css$/i,
+          use: ['style-loader', 'css-loader'],
+        },
+        {
+          test: /\.m?js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env'],
+            },
+          },
+        },
+      ],
+    },
+  };
+};
