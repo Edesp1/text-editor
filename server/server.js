7e8f89586d461b4ed008556136732b1ff30f794b
unknown <edesp.lopez17@gmail.com>
2024-08-11 11:46:44 -0700

    site now loads with styling and logo

--- server/server.js
+++ server/server.js
@@ -1,12 +1,14 @@
 const express = require('express');
+const path = require('path');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-app.use(express.static('../client'));
+// Serve static files from the 'client' directory
+app.use(express.static(path.join(__dirname, '../client')));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 require('./routes/htmlRoutes')(app);
 
-app.listen(PORT, () => console.log(`Now listening on port: https://localhost:${PORT}`));
+app.listen(PORT, () => console.log(`Now listening on port: http://localhost:${PORT}`));
